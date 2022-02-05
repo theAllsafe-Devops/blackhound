961bd4c34b0f24fb4d08dd835f7c9d73e33eb51a
anubhav <ASabharwal@tsys.com>
2022-02-05 15:06:45 +0530

    Admin

---
+++ admin-rest/src/data/upload/use-upload.mutation.ts
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from "react-query";
+import Attachment from "@repositories/upload";
+import { API_ENDPOINTS } from "@utils/api/endpoints";
+
+export const useUploadMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    (input: any) => {
+      console.log(input);
+      return Attachment.upload(API_ENDPOINTS.ATTACHMENTS, input);
+    },
+    {
+      // Always refetch after error or success:
+      onSettled: () => {
+        queryClient.invalidateQueries(API_ENDPOINTS.SETTINGS);
+      },
+    }
+  );
+};
---
+++ admin-rest/src/pages/[shop]/products/create.tsx
@@ -0,0 +1,28 @@
+import ShopLayout from "@components/layouts/shop";
+import CreateOrUpdateProductForm from "@components/product/product-form";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { adminOwnerAndStaffOnly } from "@utils/auth-utils";
+import Layout from "@components/layouts/admin";
+
+export default function CreateProductPage() {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <div className="py-5 sm:py-8 flex border-b border-dashed border-border-base">
+        <h1 className="text-lg font-semibold text-heading">
+          {t("form:form-title-create-product")}
+        </h1>
+      </div>
+      <CreateOrUpdateProductForm />
+    </>
+  );
+}
+CreateProductPage.Layout = Layout;
+
+export const getServerSideProps = async ({ locale }: any) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ["form", "common"])),
+  },
+});
