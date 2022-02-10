b66aceadb02e396269fc8836c4eb94ba77c3c10d
anubhav <ASabharwal@tsys.com>
2022-02-10 16:24:49 +0530

    Latest

--- admin-rest/next.config.js
+++ admin-rest/next.config.js
@@ -18,6 +18,7 @@ module.exports = withPWA({
       "18.141.64.26",
       "127.0.0.1",
       "localhost",
+      "35.170.79.182",
       "picsum.photos",
       "pickbazar-sail.test",
       "pickbazarlaravel.s3.ap-southeast-1.amazonaws.com",
